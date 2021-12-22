563b2626d5ec6a96baa315252fb5ea511bc88f0c
jrpolesi <josericardo.kremer@gmail.com>
2021-12-21 22:23:00 -0300

    Correct hooks

--- client/src/Contexts/GameContext/index.jsx
+++ client/src/Contexts/GameContext/index.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { io } from 'socket.io-client'
 
 
@@ -7,7 +7,7 @@ const GameContext = createContext()
 
 function GameProvider(props) {
   const [socket, setSocket] = useState()
-  const [room, setRoom] = useState(useParams().roomId)
+  const [room] = useState(useParams().roomId)
 
   useEffect(() => {
     const connection = io('/')
@@ -17,17 +17,17 @@ function GameProvider(props) {
   }, [])
 
   useEffect(() => {
-    if(socket){
+    if (socket) {
       console.log(socket)
       socket.emit('join-room', room, (err) => {
-        if(err){
+        if (err) {
           window.location.href = "/"
         }
       })
     }
   }, [socket, room])
 
-  const values = {socket, room}
+  const values = { socket, room }
   return (
     <GameContext.Provider value={values}>
       {props.children}
