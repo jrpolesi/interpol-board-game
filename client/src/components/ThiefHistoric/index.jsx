686ca41ce4ff233b3ce8f4500d56e2e856501907
jrpolesi <josericardo.kremer@gmail.com>
2021-12-25 15:43:33 -0300

    Stylize thief historic

--- client/src/components/ThiefHistoric/index.jsx
+++ client/src/components/ThiefHistoric/index.jsx
@@ -1,21 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { GameContext } from '../../Contexts/GameContext'
+import { Container } from './style'
 
 
 export function ThiefHistoric() {
-
+  const [showList, setShowList] = useState(false)
   const { thiefMovements } = useContext(GameContext)
 
+  function toggleListVisibility() {
+    setShowList(prevState => !prevState)
+  }
+
   return (
-    <section>
-      <h3>Ultimos transportes</h3>
-      <ul>
-        {thiefMovements.map(({ round, vehicle }) => (
-          <li key={round}>
-            <span>{round}</span>
-            <span>{vehicle}</span>
-          </li>))}
-      </ul>
-    </section>
+    <Container>
+      <h3 onClick={toggleListVisibility}
+        className={showList ? 'up' : ''}>
+        Histórico do ladrão
+      </h3>
+      {showList &&
+        <ul>
+          {thiefMovements.map(({ round, vehicle }) => (
+            <li key={round}>
+              <span className='thief-historic__round'>Round {round}: </span>
+              <span className='thief-historic__vehicle'>{vehicle}</span>
+            </li>))}
+        </ul>}
+    </Container>
   )
 }
--- client/src/components/ThiefHistoric/style.js
+++ client/src/components/ThiefHistoric/style.js
@@ -1,5 +1,60 @@
-import style from 'styled-components'
+import styled from 'styled-components'
 
-export const Container = style.div`
-  
+export const Container = styled.div`
+  margin-top: 5px;
+  border-radius: 5px;
+  padding: 10px;
+  width: 230px;
+  text-align: left;
+  background: white;
+
+  h3{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    font-size: 20px;
+    font-weight: bold;
+
+    &::after{
+      content: '';
+      display: inline-block;
+      border-top: 10px solid black;
+      border-left: 10px solid transparent;
+      border-right: 10px solid transparent;
+      transition: transform .1s;
+    }
+
+    &.up::after{
+      transform: rotate(180deg);
+      transition: transform .1s;
+    }
+  }
+
+  ul{
+    margin-top: 10px;
+    padding-left: 10px;
+    li{ 
+      margin-top: 6px;
+      font-size: 18px;
+
+      .thief-historic__round{
+
+        &::before{
+          content:'';
+          display: inline-block;
+          width: 5px;
+          height: 5px;
+          border-radius: 50%;
+          background-color: black;
+          vertical-align: 3px;
+          margin-right: 5px;
+        }
+      }
+
+      .thief-historic__vehicle{
+        text-transform: uppercase;
+        font-weight: bold;
+      }
+    }
+  }
 `
