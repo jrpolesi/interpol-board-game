0b5ce12564e56d2e46bf06bac7f4b70bfda9be18
jrpolesi <josericardo.kremer@gmail.com>
2021-12-25 16:49:52 -0300

    change path.join to path.resolve

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const roomsController = require('./controllers/roomsController')
 const roomRoutes = require('./routes/roomRoutes')
 app.use('/rooms', roomRoutes)
 
-app.use('/', express.static(path.join(__dirname, "client/build")))
-app.use('/:roomId', express.static(path.join(__dirname, "client/build")))
+app.use('/', express.static(path.resolve(__dirname, "client/build")))
+app.use('/:roomId', express.static(path.resolve(__dirname, "client/build")))
 
 const { Server } = require('socket.io')
 
